f141f7c2107441fe4b35c536d6420e63ca665930
Samyak Jain <samyak.jn2431@gmail.com>
2024-09-18 13:59:20 +0530

    from new folder

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void update(int *ptr){
+    
+    *ptr = *ptr +1;    
+    
+}
+
+int main(){
+
+    int a = 5;
+    int *ptr = &a;
+    cout<<"before"<<*ptr<<endl;
+    update(ptr);
+    cout<<"after"<<*ptr<<endl;
+
+}
