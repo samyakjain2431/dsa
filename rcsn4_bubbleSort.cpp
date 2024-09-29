f96fec44b4d018b10e309cb6442499f3d49cddcc
Samyak Jain <samyak.jn2431@gmail.com>
2024-09-29 19:40:32 +0530

    fixed bubblesort

--- rcsn4_bubbleSort.cpp
+++ rcsn4_bubbleSort.cpp
@@ -2,30 +2,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> bubbleSort(vector<int> &arr, int end = -1){
+vector<int> bubbleSort(vector<int> &arr, int end = -101){
     if(arr.empty()) return arr;
-    if(end == -1) end = arr.size()-1;
-    if(end ==1 || end == 0 || end==2) return arr;
+    if(end == -101) end = arr.size();
 
-    // end = arr.size() -1;
-    
-    for(int i = 0; i< end; i++){
-        if(arr[i]>arr[i+1]) swap(arr[i],arr[i+1]);
+    if(end == 1) return arr;
+
+    for(int i = 0; i < end - 1; i++){
+        if(arr[i] > arr[i+1]) swap(arr[i], arr[i+1]);
     }
     
-    return bubbleSort(arr, end-1);
+    return bubbleSort(arr, end - 1);
 }
 
 int main(){
-
-    vector<int> arr = {123,123};
+    vector<int> arr = {123, 3};
     vector<int> newArr = bubbleSort(arr);
-    for (int i = 0; i < newArr.size(); i++)
-    {
-        cout<<newArr[i]<<" ";
+    for (int i = 0; i < newArr.size(); i++) {
+        cout << newArr[i] << " ";
     }
-    cout<<endl; 
-    
+    cout << endl;
 
     return 0;
 }
